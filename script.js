c23f91a95d6a103a76d153f6415e802b17f92bff
Gurov Evgeniy <pistoleta226@yandex.ru>
2022-08-31 20:55:27 +0300

    update, fix UI bugs

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const input = document.querySelector('.input')
-const text  = document.querySelector('.text')
-const btn   = document.querySelector('.btn')
+const text = document.querySelector('.text')
+const btn = document.querySelector('.btn')
 const emojis = ['^_^', ':3', '<3', 'T_T', ':*', ":'("]
 let min = 0
 let max = 1
 
 function convertToEmo() {
-    return input.value.replaceAll('мне ', 'мну ').split('').map((c, i) =>
-        i % 2 ? c.toLowerCase() : c.toUpperCase()).join('')
+  return input.value.replaceAll('мне ', 'мну ').split('').map((c, i) =>
+    i % 2 ? c.toLowerCase() : c.toUpperCase()).join('')
 }
 
 function copyToClipboard() {
-    window.navigator.clipboard.writeText(text.textContent)
+  window.navigator.clipboard.writeText(text.textContent)
 }
 
 function showAlert() {
@@ -31,44 +31,51 @@ function showAlert() {
 }
 
 function randomEmoji() {
-    if (input.value !== text.textContent) {
-        return emojis[Math.floor(Math.random() * emojis.length)]
-    } else {
-        return ''
-    }
+  if (input.value !== text.textContent) {
+    return emojis[Math.floor(Math.random() * emojis.length)]
+  } else {
+    return ''
+  }
 }
 
 function clear() {
-    input.select()
-    input.value = ''
-    if (window.innerWidth >= 375) {
-        input.focus()
-        return
-    }
-    input.blur()
+  input.select()
+  input.value = ''
+  if (window.innerWidth >= 375) {
+    input.focus()
+    return
+  }
+  input.blur()
 }
 
 window.addEventListener('keydown', (event) => {
-    if (event.code === 'Enter' && input.value) {
-        text.innerHTML = convertToEmo() + randomEmoji()
-        copyToClipboard()
-        showAlert()
-        clear()
-    }
+  if (event.code === 'Enter' && input.value) {
+    text.innerHTML = convertToEmo() + randomEmoji()
+    copyToClipboard()
+    showAlert()
+    clear()
+  }
 })
 
 btn.addEventListener('click', () => {
-    if (input.value) {
-        text.innerHTML = convertToEmo() + randomEmoji()
-            if (window.innerWidth >= 375) {
-            copyToClipboard()
-            showAlert()
-        }
-        clear()
+  if (input.value) {
+    text.innerHTML = convertToEmo() + randomEmoji()
+    if (window.innerWidth >= 375) {
+      copyToClipboard()
+      showAlert()
     }
+    clear()
+  }
 })
 
 text.addEventListener('click', () => {
-    copyToClipboard()
-    showAlert()
+  copyToClipboard()
+  showAlert()
+})
+
+input.addEventListener('input', () => {
+  if (input.value.length <= 90) {
+    input.value.slice(0, 90)
+    counter.textContent = input.value.split('').length
+  }
 })
